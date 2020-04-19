d7b4b34a7f2ed2c1dcbe87d3aafff79b370ed641
Alexander Low <alexanderlow@MacBook-Air.local>
2020-04-19 19:53:17 -0400

    fix: added logic to handle menu toggle

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import Logo from "../images/logo.png"
 
-const Header = () => {
+const Navbar = () => {
+  const [menu, menuToggle] = useState(false)
+
   return (
     <nav className="navbar is-fixed-top">
       <div className="container">
@@ -14,30 +16,53 @@ const Header = () => {
               alt="Aluminum Associates logo"
             />
           </Link>
-          <div id="burger" className="navbar-burger">
+          <div
+            id="burger"
+            className={"navbar-burger" + (menu ? " is-active" : "")}
+            onClick={() => menuToggle(!menu)}
+          >
             <span></span>
             <span></span>
             <span></span>
           </div>
         </div>
-        <div id="nav-menu" className="navbar-menu">
+        <div
+          id="nav-menu"
+          className={"navbar-menu" + (menu ? " is-active" : "")}
+        >
           <div className="navbar-end">
             <Link to="/" className="navbar-item" activeClassName="is-active">
               Home
             </Link>
-            <Link to="/about" className="navbar-item" activeClassName="is-active">
+            <Link
+              to="/about"
+              className="navbar-item"
+              activeClassName="is-active"
+            >
               About
             </Link>
-            <Link to="/products" className="navbar-item" activeClassName="is-active">
+            <Link
+              to="/products"
+              className="navbar-item"
+              activeClassName="is-active"
+            >
               Products
             </Link>
-            <Link to="/services" className="navbar-item" activeClassName="is-active">
+            <Link
+              to="/services"
+              className="navbar-item"
+              activeClassName="is-active"
+            >
               Services
             </Link>
             <Link to="/faq" className="navbar-item" activeClassName="is-active">
               FAQ
             </Link>
-            <Link to="/contact" className="navbar-item" activeClassName="is-active">
+            <Link
+              to="/contact"
+              className="navbar-item"
+              activeClassName="is-active"
+            >
               Contact
             </Link>
           </div>
@@ -47,4 +72,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default Navbar
