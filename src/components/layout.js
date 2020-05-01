3091217c6a52802268b8cea47d9a2bda035724eb
Alexander Low <alexanderlow@MacBook-Air.local>
2020-05-01 16:00:41 -0400

    refactor: moved seo to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import SEO from "./seo"
 import Navbar from "./navbar"
 import Footer from "./footer"
 
 const Layout = props => {
   return (
     <>
+      <SEO title={props.title} description={props.description}></SEO>
       <Helmet
         bodyAttributes={{
           class: "has-navbar-fixed-top",
