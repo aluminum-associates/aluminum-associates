7579193ebc044d098909e11b64b3abeb30f86166
XanderJL <alexanderjameslow@gmail.com>
2020-05-29 14:47:04 -0400

    refactored: listed imports alphabetically

--- schemas/schema.js
+++ schemas/schema.js
@@ -2,28 +2,28 @@ import createSchema from "part:@sanity/base/schema-creator";
 import schemaTypes from "all:part:@sanity/base/schema-type";
 
 import blockContent from "./blockContent";
-import category from "./category";
-import product from "./product";
-import vendor from "./vendor";
-import page from "./page";
-import galleryImage from "./galleryImage";
-import productDocument from "./productDocument";
-import heroImage from "./heroImage";
 import blockImage from "./blockImage";
+import category from "./category";
+import galleryImage from "./galleryImage";
+import heroImage from "./heroImage";
+import page from "./page";
+import product from "./product";
+import productDocument from "./productDocument";
+import vendor from "./vendor";
 import youtube from "./youtube";
 
 export default createSchema({
   name: "default",
   types: schemaTypes.concat([
-    product,
-    vendor,
-    category,
-    page,
     blockContent,
-    galleryImage,
-    productDocument,
-    heroImage,
     blockImage,
+    category,
+    galleryImage,
+    heroImage,
+    page,
+    product,
+    productDocument,
+    vendor,
     youtube,
   ]),
 });
