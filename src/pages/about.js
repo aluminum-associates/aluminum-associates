43311686abcd29d4518b29f1dab77602a5ad4990
Alexander Low <alexanderlow@MacBook-Air.local>
2020-07-20 12:08:18 -0400

    refactored: about page is now editable in sanity

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,149 +1,75 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import BackgroundImage from "gatsby-background-image"
+import PortableText from "@sanity/block-content-to-react"
 import Layout from "../components/Layout"
+import HeroOverlay from "../components/HeroOverlay"
 
-export default function About() {
+const About = ({ data }) => {
   const copyStyle = {
     maxWidth: "70ch",
+  }
+  const {
+    title,
+    metaDescription,
+    _rawHeroCopy,
+    _rawBody,
+    heroImage,
+  } = data.sanityAbout
+
+  const BlockRenderer = props => {
+    const { style = "normal" } = props.node
+
+    if (/^h\d/.test(style)) {
+      const level = style.replace(/[^\d]/g, "")
+      return React.createElement(
+        style,
+        { className: `title is-size-2` },
+        props.children
+      )
+    }
+
+    // Fall back to default handling
+    return PortableText.defaultSerializers.types.block(props)
   }
 
   return (
     <div>
-      <Layout title="About">
-        <section className="hero is-medium is-primary">
-          <div className="hero-body">
-            <div className="container">
-              <div className="content" style={copyStyle}>
-                <h1 className="title is-size-2">Since 1963</h1>
-                <p>
-                  Aluminum Associates, founded in 1963, is a leading provider of
-                  exterior home improvement products and services in the London
-                  area – and has been for over five decades!
-                </p>
-                <p>
-                  Its founders, Jim and Betty Low, began operations from their
-                  back porch and expanded the company by extending their family
-                  values of honesty, accountability, dedication, and caring to
-                  each and every business transaction.
-                </p>
-                <p>
-                  Today, their son, Ian, is at the helm and Aluminum Associates
-                  continues to thrive. Every new undertaking is more than just
-                  another job – it`s an opportunity for Ian to honour his
-                  parent`s legacy by practicing the values he was raised with.
-                </p>
+      <Layout title={title} description={metaDescription}>
+        {heroImage ? (
+          <BackgroundImage
+            fluid={heroImage.asset.fluid}
+            className="hero is-medium is-primary"
+          >
+            <HeroOverlay>
+              <div className="hero-body">
+                <div className="container">
+                  <div className="content">
+                    <PortableText
+                      blocks={_rawHeroCopy}
+                      serializers={{ types: { block: BlockRenderer } }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </HeroOverlay>
+          </BackgroundImage>
+        ) : (
+          <div className="hero is-medium is-primary">
+            <div className="hero-body">
+              <div className="container">
+                <div className="content" style={copyStyle}>
+                  <PortableText blocks={_rawHeroCopy} />
+                </div>
               </div>
             </div>
           </div>
-        </section>
+        )}
         <section className="section">
           <div className="container">
             <div className="content" style={copyStyle}>
-              <h2 className="title is-size-3 is-size-4-mobile">President's Message</h2>
-              <p>
-                "Aluminum Associates started with a simple business philosophy –
-                provide quality products at reasonable prices with customer
-                service and satisfaction as our first priority. Today, over five
-                decades later, this philosophy is still in practice – it`s the
-                code by which we operate and the key to our success."
-              </p>
-              <p>Ian Low,</p>
-              <p>President &amp; General Manager</p>
-            </div>
-          </div>
-        </section>
-        <section className="section">
-          <div className="container">
-            <div className="content" style={copyStyle}>
-              <h2 className="title is-size-3 is-size-4-mobile">What Sets Us Apart</h2>
-              <p>
-                Our business continues to thrive because we demonstrate, on a
-                daily basis, that we value the same things our customers do.
-                Easy for us to say perhaps, but our customers say the same thing
-                – see the Testimonials page to see what they`ve said.
-              </p>
-            </div>
-          </div>
-        </section>
-        <section className="section">
-          <div className="container">
-            <div className="content" style={copyStyle}>
-              <h2 className="title is-size-3 is-size-4-mobile">Our Values</h2>
-              <p>
-                Honesty - We offer fair pricing and no-pressure sales;
-                everything is as straightforward as possible, and we put it in
-                writing to ensure that the details are clearly understood.
-              </p>
-              <p>
-                Integrity - We sell only quality products that are backed with
-                solid warranties – as is our installation service. Our
-                installers demonstrate professional workmanship – the same
-                workmanship they would use on their own homes. Our customers
-                receive written assurances of quality products and installation,
-                giving them peace of mind for the years ahead.
-              </p>
-              <p>
-                Respect - Our customers are always treated with courtesy, their
-                questions and concerns are handled with compassion, and their
-                property treated with great care.
-              </p>
-              <p>
-                Dedication - Our knowledgeable, skilled staff is dedicated to
-                serving the needs of our customers – they demonstrate this
-                through their attention to detail and the pride they have in
-                their work. Our employees get a great deal of pleasure from
-                helping customers find the product they`re searching for, or
-                completing an installation that makes an improvement in a
-                customer`s home.
-              </p>
-              <p>
-                Professionalism - Our company vehicles are kept clean and in
-                good working order, and our staff wear uniforms so that
-                customers can easily identify them as members of our qualified
-                team. Our staff receive thorough training and supervision to
-                ensure quality of service and workmanship is always maintained.
-              </p>
-              <p>
-                Most of our customers come to us as a result of referrals from
-                friends or neighbours of customers who have experienced the
-                values that Aluminum Associates operates by. We operate our
-                business with every intention of being here to serve you, our
-                customers, for decades to come.
-              </p>
-            </div>
-          </div>
-        </section>
-        <section className="section">
-          <div className="container">
-            <div className="content" style={copyStyle}>
-              <h2 className="title is-size-3 is-size-4-mobile">Our Community</h2>
-              <p>
-                The work that we do beautifies the homes and neighbourhoods in our
-                community. We take pride in knowing that we also take the steps
-                needed to ensure that we keep our community beautiful by recycling
-                as much waste from our office, warehouse and job sites as
-                possible. At Aluminum Associates we have a number of large
-                recycling bins on our property where we collect waste aluminum,
-                vinyl, and cardboard. These, along with used doors and windows,
-                are directed to recycling depots where they will be used to create
-                a variety of post-consumer products, rather than filling up our
-                city`s landfill. Our actions demonstrate our dedication to take
-                the steps necessary to protect our environment.
-              </p>
-              <p>
-                Serving London and area since 1963, we have always believed that
-                it is both a pleasure and a responsibility to give back to the
-                community in which we live and work. Each year Aluminum Associates
-                donates to local community groups, especially those that offer
-                opportunities for young children to participate in sports, or for
-                women and children in crisis.
-              </p>
-              <p>
-                The company provides employment for more than 25 employees who
-                subsequently provide for their families. We also supply products
-                to a large number of contractors who make their living in the
-                exterior home improvement industry. Together, we make a
-                significant contribution to the economy of our community.
-              </p>
+              <PortableText blocks={_rawBody} />
             </div>
           </div>
         </section>
@@ -151,3 +77,23 @@ export default function About() {
     </div>
   )
 }
+
+export const data = graphql`
+  {
+    sanityAbout {
+      title
+      metaDescription
+      _rawBody
+      _rawHeroCopy
+      heroImage {
+        asset {
+          fluid {
+            ...GatsbySanityImageFluid
+          }
+        }
+      }
+    }
+  }
+`
+
+export default About
