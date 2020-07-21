2b67cc6c275611d15fac51584e84e660557e0ca0
XanderJL <alexanderjameslow@gmail.com>
2020-07-21 13:23:39 -0400

    refactored: made services page editable from sanity

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,138 +1,47 @@
 import React from "react"
+import { graphql } from "gatsby"
+import PortableText from "@sanity/block-content-to-react"
 import Layout from "../components/Layout"
+import Hero from "../components/Hero"
 
-export default function Services() {
+export default function Services({ data }) {
+  const {
+    title,
+    metaDescription,
+    heroImage,
+    heroSize,
+    _rawBody,
+  } = data.sanityServices
   return (
-    <Layout title="Services">
-      <div className="hero is-medium is-primary">
-        <div className="hero-body">
-          <div className="container">
-            <h1 className="title is-size-2">Services</h1>
-          </div>
-        </div>
-      </div>
+    <Layout title={title} description={metaDescription}>
+      <Hero size={heroSize} fluid={heroImage ? heroImage.asset.fluid : null}>
+        <h1 className="title is-size-2">{title}</h1>
+      </Hero>
       <section className="section">
         <div className="container">
           <div className="content">
-            <h2 className="title is-size-3 is-size-4-mobile">Free, No Obligation Estimates</h2>
-            <p>
-              Our Outside Sales Representatives will be happy to provide a free,
-              in-home consultation for homeowners, while builders and property
-              management companies can request a quote from blue prints or an
-              on-site visit. Our knowledgeable and experienced staff can explain
-              all the options, ensure that you have the right product for your
-              application, and provide a no obligation, no hassle, and no
-              pressure quote, at a competitive price.
-            </p>
-          </div>
-        </div>
-      </section>
-      <section className="section">
-        <div className="container">
-          <div className="content">
-            <h2 className="title is-size-3 is-size-4-mobile">Professional Installation</h2>
-            <p>
-              Aluminum Associates provides professional installation services
-              for those customers who want the security of knowing that their
-              project has been completed to the highest standards, and the work
-              is guaranteed.
-            </p>
-            <p>
-              Our Installation Manager takes the time to recheck all site
-              measurements before the product is ordered, so that the person
-              responsible for the installation is involved at an early stage. In
-              this way we have a smooth transition from sales, to ordering, to
-              installation – ensuring the product is installed to the customer`s
-              complete satisfaction.
-            </p>
-            <p>
-              Unique to Aluminum Associates, our custom designed installation
-              trucks have been laid out to provide the highest level of
-              efficiency while on the job site. These 16` cube vans are fully
-              equipped workshops on wheels at your doorstep
-            </p>
-          </div>
-        </div>
-      </section>
-      <section className="section">
-        <div className="container">
-          <div className="content">
-            <h2 className="title is-size-3 is-size-4-mobile">
-              For The Contractor / Do-It-Yourselfer
-            </h2>
-            <p>
-              We have an extensive line of in-stock vinyl and aluminum products,
-              as well as the accessories, tools, and fasteners needed to
-              complete your project. Our friendly and knowledgeable sales
-              counter staff know these products inside and out, and they are
-              eager to help. Provision of how-to installation guides, equipment
-              rental, and delivery service are just some of the services that we
-              offer to assist you. See the Contractor / DIY page of this website
-              for more information.
-            </p>
-          </div>
-        </div>
-      </section>
-      <section className="section">
-        <div className="container">
-          <div className="content">
-            <h2 className="title is-size-3 is-size-4-mobile">Accessible Customer Service</h2>
-            <p>
-              At Aluminum Associates we want to ensure that our services are
-              readily available to customers who may have a disability.
-            </p>
-            <p>
-              Those customers with physical disabilities who want to visit our
-              showroom will be happy to know that the entrance to our showroom
-              is at ground level and has double doors. The display area has wide
-              aisles and good lighting, and our washrooms were designed to
-              accommodate wheelchairs. We welcome support persons and service
-              animals.
-            </p>
-            <p>
-              All of our customer service reps have received training in
-              accessible customer service. If we have any planned or unplanned
-              disruption in service we will post a notice on the Contact page of
-              this website and at the entrance to our showroom.
-            </p>
-            <p>
-              If you would like further information about our Accessible
-              Customer Service Policy, click on the following link, or if you
-              would like to comment on our policy please{" "}
-              <a
-                href="mailto:gm@aluminumassociates.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                contact our General Manager
-              </a>
-              .
-            </p>
-          </div>
-        </div>
-      </section>
-      <section className="section">
-        <div className="container">
-          <div className="content">
-            <h3 className="title is-size-3 is-size-4-mobile">
-              Aluminum Associates Accessible Service Policy.
-            </h3>
-            <p>
-              If you have a question about any of the products that we sell, if
-              you`d like to receive a free estimate for professional
-              installation, or ask about a project you`re going to install
-              yourself, don`t hesitate to contact us – we`re happy to help. You
-              can speak to a sales representative in our showroom at 1801
-              Trafalgar St. East, call us at{" "}
-              <a href="tel:+1(519)453-6400">519-453-6400</a>, or{" "}
-              <a href="mailto:website@aluminumassociates.com">
-                click here to e-mail us
-              </a>
-              .
-            </p>
+            <PortableText blocks={_rawBody} />
           </div>
         </div>
       </section>
     </Layout>
   )
 }
+
+export const data = graphql`
+  {
+    sanityServices {
+      title
+      metaDescription
+      heroImage {
+        asset {
+          fluid(maxWidth: 1920) {
+            ...GatsbySanityImageFluid
+          }
+        }
+      }
+      heroSize
+      _rawBody
+    }
+  }
+`
