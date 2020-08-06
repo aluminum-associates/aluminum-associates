b0fcb7e15c5d8789bc1bf16398a3422e3cc9dd5a
XanderJL <alexanderjameslow@gmail.com>
2020-08-06 13:22:28 -0400

    refactored: animation timing

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import PortableText from "@sanity/block-content-to-react"
-import { CSSTransition } from "react-transition-group"
+import { motion, AnimatePresence } from "framer-motion"
 
 const Notification = () => {
   const [closed, setClosed] = useState(false)
@@ -19,31 +19,47 @@ const Notification = () => {
     }
   `)
   const { _rawNotification } = query.sanityIndex
+  const variants = {
+    hidden: {
+      y: 30,
+      opacity: 0,
+    },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        type: "tween",
+      },
+    },
+  }
 
   return (
-    <CSSTransition
-      in={!closed}
-      timeout={300}
-      unmountOnExit
-      classNames="notification"
-      onEnter={() => setClosed(!closed)}
-    >
-      <div className="notification is-warning">
-        <button
-          aria-label="Close notification"
-          className="delete"
-          onClick={() => setClosed(!closed)}
-          ref={notificationRef}
-          tabIndex={-1}
-          onKeyDown={e => (e.key === "Escape" ? setClosed(!closed) : null)}
-        ></button>
-        <div className="container">
-          <div className="content">
-            <PortableText blocks={_rawNotification} />
+    <AnimatePresence>
+      {!closed && (
+        <motion.div
+          className="notification is-warning"
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+          variants={variants}
+        >
+          <button
+            aria-label="Close notification"
+            className="delete"
+            onClick={() => setClosed(!closed)}
+            ref={notificationRef}
+            tabIndex={-1}
+            onKeyDown={e => (e.key === "Escape" ? setClosed(!closed) : null)}
+          ></button>
+          <div className="container">
+            <div className="content">
+              <PortableText blocks={_rawNotification} />
+            </div>
           </div>
-        </div>
-      </div>
-    </CSSTransition>
+        </motion.div>
+      )}
+    </AnimatePresence>
   )
 }
 
