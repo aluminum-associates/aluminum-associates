10a25daadfbd479e6cf2328a61ba4eb50a6620ca
XanderJL <alexanderjameslow@gmail.com>
2020-08-06 16:47:40 -0400

    refactored: new product template

--- src/templates/product.js
+++ src/templates/product.js
@@ -32,48 +32,26 @@ const Product = ({ data }) => {
       </Hero>
       <div className="container" style={{ maxWidth: "960px" }}>
         <section className="section">
-          {images && images.length !== 0 ? (
-            <div className="product-wrapper">
+          <div className="product-wrapper">
+            {images && images.length > 0 ? (
               <ImageGallery images={images} />
-              <div className="product-copy">
-                <h2 className="title is-size-3 is-size-4-mobile">{title}</h2>
-                <h3 className="subtitle is-size-6">
+            ) : null}
+            <div className="product-copy">
+              <h2 className="title is-size-3 is-size-4-mobile">{title}</h2>
+              <h3 className="subtitle is-size-6">
+                {vendor && (
                   <a
                     href={vendor.url}
                     target="_blank"
                     rel="noopener noreferrer"
                   >
                     {vendor.title}
-                  </a>{" "}
-                  -{" "}
-                  <Link to={`/products/${category.slug.current}`}>
-                    {category.title}
-                  </Link>
-                </h3>
-                <PortableText blocks={_rawDescription} />
-                {standardFeatures || optionalFeatures ? (
-                  <div className="menus my-2">
-                    {standardFeatures.length > 0 && (
-                      <Accordion
-                        title="Standard Features"
-                        list={standardFeatures}
-                      />
-                    )}
-                    {optionalFeatures.length > 0 && (
-                      <Accordion
-                        title="Optional Features"
-                        list={optionalFeatures}
-                      />
-                    )}
-                  </div>
-                ) : null}
-              </div>
-            </div>
-          ) : (
-            <div className="product-copy">
-              <h2 className="title is-size-3 is-size-4-mobile">{title}</h2>
-              <h3 className="subtitle is-size-4 is-size-5-mobile">
-                {vendor.title}
+                  </a>
+                )}{" "}
+                -{" "}
+                <Link to={`/products/${category.slug.current}`}>
+                  {category.title}
+                </Link>
               </h3>
               <PortableText blocks={_rawDescription} />
               {standardFeatures || optionalFeatures ? (
@@ -93,12 +71,30 @@ const Product = ({ data }) => {
                 </div>
               ) : null}
             </div>
-          )}
+          </div>
         </section>
-        <hr style={{ border: "1px solid #CFCFCF", margin: "1.5rem" }} />
+        {_rawAdditionalInfo && (
+          <>
+            <hr style={{ border: "1px solid #CFCFCF", margin: "1.5rem" }} />
+            <section className="section">
+              <h2 className="title is-size-3 is-size-4-mobile">More Detail</h2>
+              <PortableText blocks={_rawAdditionalInfo} />
+            </section>
+          </>
+        )}
         <section className="section">
-          <h2 className="title is-size-3 is-size-4-mobile">More Detail</h2>
-          <div className="details-wrapper"></div>
+          <p className="disclaimer">
+            * If you have a question about any of the products that we sell, if
+            you'd like to receive a free estimate for professional installation,
+            or have questions about a product you're going to install yourself,
+            don't hesitate to contact us — we're happy to help. You can speak to
+            a sales representative in our showroom at 1801 Trafalgar St. East,
+            call us at <a href="tel:+(519)453-6400">519-453-6400</a>, or{" "}
+            <a href="mailto:info@aluminumassociates.com">
+              click here to e-mail us
+            </a>
+            .
+          </p>
         </section>
       </div>
     </Layout>
