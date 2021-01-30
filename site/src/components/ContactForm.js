e5f950cd279be1f102ad5f6da39db58b61ac9b59
Alex Low <me@alexlow.dev>
2021-01-29 20:28:01 -0500

    changes: thank you modal

--- site/src/components/ContactForm.js
+++ site/src/components/ContactForm.js
@@ -9,6 +9,14 @@ import {
   Heading,
   Textarea,
   Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
 } from "@chakra-ui/react"
 
 const encode = data => {
@@ -39,7 +47,8 @@ const inputStyles = {
 
 //Form Component
 const ContactForm = ({ title }) => {
-  const { register, handleSubmit, errors } = useForm()
+  const { register, handleSubmit, errors, reset } = useForm()
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   // Validation rules
   const validateName = value => {
@@ -87,98 +96,114 @@ const ContactForm = ({ title }) => {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", ...data }),
     })
-      .then(res => console.log(res))
+      .then(res => {
+        reset()
+        onOpen()
+      })
       .catch(err => console.log(err))
   }
 
   return (
-    <Box flex={1} maxW="50ch">
-      <Heading as="h2" pb="1rem">
-        {title}
-      </Heading>
-      <form
-        name="contact"
-        method="post"
-        data-netlify="true"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <input type="hidden" name="form-name" value="contact" />
-        <FormControl isInvalid={errors.name} {...controlStyles}>
-          <FormLabel htmlFor="name" {...labelStyles}>
-            Name
-          </FormLabel>
-          <Input
-            name="name"
-            placeholder="Jane Doe"
-            ref={register({ validate: validateName })}
-            {...inputStyles}
-          />
-          <FormErrorMessage>
-            {errors.name && errors.name.message}
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl isInvalid={errors.address} {...controlStyles}>
-          <FormLabel htmlFor="address" {...labelStyles}>
-            Address
-          </FormLabel>
-          <Input
-            name="address"
-            placeholder="1801 Trafalgar St. East"
-            ref={register({ validate: validateAddress })}
-            {...inputStyles}
-          />
-          <FormErrorMessage>
-            {errors.address && errors.address.message}
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl isInvalid={errors.email} {...controlStyles}>
-          <FormLabel htmlFor="email" {...labelStyles}>
-            Email
-          </FormLabel>
-          <Input
-            name="email"
-            placeholder="jane.d@email.com"
-            ref={register({ validate: validateEmail })}
-            {...inputStyles}
-          />
-          <FormErrorMessage>
-            {errors.email && errors.email.message}
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl isInvalid={errors.phone} {...controlStyles}>
-          <FormLabel htmlFor="phone" {...labelStyles}>
-            Phone Number
-          </FormLabel>
-          <Input
-            name="phone"
-            placeholder="519-123-4567"
-            ref={register({ validate: validatePhone })}
-            {...inputStyles}
-          />
-          <FormErrorMessage>
-            {errors.phone && errors.phone.message}
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl isInvalid={errors.message} {...controlStyles}>
-          <FormLabel htmlFor="message" {...labelStyles}>
-            Message
-          </FormLabel>
-          <Textarea
-            name="message"
-            placeholder="Hello!"
-            rows={10}
-            ref={register({ validate: validateMessage })}
-            {...inputStyles}
-          />
-          <FormErrorMessage>
-            {errors.message && errors.message.message}
-          </FormErrorMessage>
-        </FormControl>
-        <Button type="submit" colorScheme="primary">
-          Submit
-        </Button>
-      </form>
-    </Box>
+    <>
+      <Box flex={1} maxW="50ch">
+        <Heading as="h2" pb="1rem">
+          {title}
+        </Heading>
+        <form
+          name="contact"
+          method="post"
+          data-netlify="true"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <FormControl isInvalid={errors.name} {...controlStyles}>
+            <FormLabel htmlFor="name" {...labelStyles}>
+              Name
+            </FormLabel>
+            <Input
+              name="name"
+              placeholder="Jane Doe"
+              ref={register({ validate: validateName })}
+              {...inputStyles}
+            />
+            <FormErrorMessage>
+              {errors.name && errors.name.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={errors.address} {...controlStyles}>
+            <FormLabel htmlFor="address" {...labelStyles}>
+              Address
+            </FormLabel>
+            <Input
+              name="address"
+              placeholder="1801 Trafalgar St. East"
+              ref={register({ validate: validateAddress })}
+              {...inputStyles}
+            />
+            <FormErrorMessage>
+              {errors.address && errors.address.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={errors.email} {...controlStyles}>
+            <FormLabel htmlFor="email" {...labelStyles}>
+              Email
+            </FormLabel>
+            <Input
+              name="email"
+              placeholder="jane.d@email.com"
+              ref={register({ validate: validateEmail })}
+              {...inputStyles}
+            />
+            <FormErrorMessage>
+              {errors.email && errors.email.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={errors.phone} {...controlStyles}>
+            <FormLabel htmlFor="phone" {...labelStyles}>
+              Phone Number
+            </FormLabel>
+            <Input
+              name="phone"
+              placeholder="519-123-4567"
+              ref={register({ validate: validatePhone })}
+              {...inputStyles}
+            />
+            <FormErrorMessage>
+              {errors.phone && errors.phone.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={errors.message} {...controlStyles}>
+            <FormLabel htmlFor="message" {...labelStyles}>
+              Message
+            </FormLabel>
+            <Textarea
+              name="message"
+              placeholder="Hello!"
+              rows={10}
+              ref={register({ validate: validateMessage })}
+              {...inputStyles}
+            />
+            <FormErrorMessage>
+              {errors.message && errors.message.message}
+            </FormErrorMessage>
+          </FormControl>
+          <Button type="submit" colorScheme="primary">
+            Submit
+          </Button>
+        </form>
+      </Box>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Thank You!</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            We've recieved your email and will get back to you as soon as
+            possible.
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
   )
 }
 
