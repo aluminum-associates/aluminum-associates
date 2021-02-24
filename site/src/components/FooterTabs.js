57eb4850a8ed7b3bb3ab268527e61c0ea433e7be
Alex Low <me@alexlow.dev>
2021-02-24 10:41:43 -0500

    refactored: footer tabs now built in chakra

--- site/src/components/FooterTabs.js
+++ site/src/components/FooterTabs.js
@@ -1,30 +1,48 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link as GatsbyLink } from "gatsby"
+import { Tabs, TabList, Tab, Link, Flex } from "@chakra-ui/react"
 import { useLocation } from "@reach/router"
 
-const FooterTabs = (props, context) => {
+const FooterTabs = props => {
   const currentPath = useLocation().pathname
 
+  const defaultIndex = props.tabs.map(tab => tab.slug).indexOf(currentPath)
+
   return (
-    <div className="hero-footer">
-      <div className="tabs is-boxed is-fullwidth">
-        <div className="container" style={{ maxWidth: "75ch" }}>
-          <ul>
-            {props.tabs.map(tab => {
-              const { title, slug } = tab
-              return (
-                <li
-                  key={title}
-                  className={currentPath === slug ? "is-active" : ""}
+    <Flex justifyContent="center" alignItems="center" overflow="hidden">
+      <Tabs
+        flex={1}
+        maxW="75ch"
+        variant="enclosed"
+        colorScheme="primary"
+        defaultIndex={defaultIndex}
+        isFitted
+      >
+        <TabList>
+          {props.tabs.map(tab => {
+            const { title, slug } = tab
+            return (
+              <Tab
+                key={title}
+                p={0}
+                _selected={{ color: "primary.800", bg: "white" }}
+                _hover={{ bg: "primary.800", color: "white" }}
+              >
+                <Link
+                  flex={1}
+                  p="8px 16px"
+                  as={GatsbyLink}
+                  to={slug}
+                  _hover={{ bg: "transparent" }}
                 >
-                  <Link to={slug}>{title}</Link>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
-      </div>
-    </div>
+                  {title}
+                </Link>
+              </Tab>
+            )
+          })}
+        </TabList>
+      </Tabs>
+    </Flex>
   )
 }
 
