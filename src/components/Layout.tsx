03d9c97c671582c7a2449406f954a7dbc72c4bbf
Xander Low <alexanderjameslow@gmail.com>
2022-05-18 23:04:33 -0700

    updated: calculated nav height is now dynamic

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
-import { Box, Flex, FlexProps } from '@chakra-ui/react'
-import { FC } from 'react'
+import { Box, Flex, FlexProps, useDimensions } from '@chakra-ui/react'
+import { FC, useRef } from 'react'
 import { NavItems } from 'types/SanityExtended'
 import Navbar from './Navbar'
 
@@ -8,10 +8,15 @@ interface LayoutProps extends FlexProps {
 }
 
 const Layout: FC<LayoutProps> = ({ children, navItems }) => {
+  const navRef = useRef<HTMLDivElement>(null)
+  const navDimensions = useDimensions(navRef)
+  const navHeight: string = navDimensions?.borderBox.height + 'px'
+
   return (
     <Flex direction='column' minH='100%'>
-      <Navbar navItems={navItems} />
-      <Box mt='72px'>{children}</Box>
+      {/* @ts-ignore can't figure out this ref bs :) */}
+      <Navbar ref={navRef} navItems={navItems} />
+      <Box mt={navHeight}>{children}</Box>
     </Flex>
   )
 }
